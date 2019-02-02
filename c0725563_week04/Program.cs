3470b509ee0163ff8afd8d2d2c2e37fade8ab599
harman <hrmnsingh9117@gmail.com>
2019-02-01 20:54:56 -0500

    c0725563_week_04

--- c0725563_week04/Program.cs
+++ c0725563_week04/Program.cs
@@ -6,10 +6,32 @@ using System.Threading.Tasks;
 
 namespace c0725563_week04
 {
+
     class Program
     {
         static void Main(string[] args)
         {
+            countryside c = new countryside();
+
         }
     }
+    class village
+    {
+        bool isAstrildeHere;
+        village nextvillage;
+        village prevvillage;
+        String villagename;
+
+
+    }
+
+
+    class countryside
+    {
+        village mapple;
+        village toronto;
+        village ajax;
+    }
 }
+
+
